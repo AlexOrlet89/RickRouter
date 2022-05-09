74d5b8421b133b6467a70776a1631e34df775e7d
Alex Orlet <AlexOrlet89@gmail.com>
2022-05-09 11:09:50 -0700

    fetch filtered returns results filtered by name

--- src/views/List.js
+++ src/views/List.js
@@ -1,14 +1,43 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function List() {
+  const [filteredCharacters, setFilteredCharacters] = useState('All');
+
   useEffect(() => {
     const fetchCharacters = async () => {
-      const res = await fetch(`https://rickandmortyapi.com/api/character`);
-      const results = await res.json();
-      console.log(results);
+      if (filteredCharacters === 'All') {
+        const res = await fetch(`https://rickandmortyapi.com/api/character`);
+        const results = await res.json();
+        console.log(results.results);
+      } else {
+        const res = await fetch(
+          `https://rickandmortyapi.com/api/character/?name=${filteredCharacters}`
+        );
+        const results = await res.json();
+        console.log(results.results);
+      }
     };
     fetchCharacters();
-  }, []);
+  }, [filteredCharacters]);
 
-  return <div>List</div>;
+  const handleFilter = (e) => {
+    // console.log(e.target.className);
+    setFilteredCharacters(e.target.className);
+    console.log(filteredCharacters);
+  };
+
+  return (
+    <>
+      <h3>Characters</h3>
+      <h5 className="All" onClick={(e) => handleFilter(e)}>
+        All
+      </h5>
+      <h5 className="Rick" onClick={(e) => handleFilter(e)}>
+        Ricks
+      </h5>
+      <h5 className="Morty" onClick={(e) => handleFilter(e)}>
+        Morties
+      </h5>
+    </>
+  );
 }
