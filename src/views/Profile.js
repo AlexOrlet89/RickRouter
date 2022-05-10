204557580c738375ca6b911b4bdeaa9dd4611c85
Alex Orlet <AlexOrlet89@gmail.com>
2022-05-10 13:40:10 -0700

    back button on profile sends back to previous path

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 
 export default function Profile() {
   const location = useLocation();
+  const history = useHistory();
 
   const { from } = location.state || { from: { pathname: '/' } };
   console.log(from);
 
+  const handleBack = () => {
+    history.replace(from);
+  };
+
   return (
     <>
-      <div>Profile</div>;
+      <div>Profile</div>
+      <button onClick={handleBack}>Back to Characters</button>
     </>
   );
 }
